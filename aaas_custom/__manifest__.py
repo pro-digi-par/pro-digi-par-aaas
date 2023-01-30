d25dc75d8116e512a79a5f437fae0547a08773ac
pro-digi-par <jdietl@pro-digi-par.de>
2023-01-30 16:48:00 +0100

    [BUGFIX]

--- aaas_custom/__manifest__.py
+++ aaas_custom/__manifest__.py
@@ -4,10 +4,10 @@
     "name": "Accounting as a Service Customizations",
     "summary": "Minor customizations for accounting as a service",
     "version": "16.0.1",
-    "author": "Pro Digi Par GmbH"
+    "author": "Pro Digi Par GmbH",
     "license": "Proprietary",
     "maintainer": "Pro Digi Par GmbH",
-    "depends": ["base_setup"]
+    "depends": ["base_setup"],
     "data": [
     ],
     "auto_install": False,
